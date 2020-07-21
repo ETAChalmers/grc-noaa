f98733655f5906d55fde5e8722a3ab8023b5b199
david <davidlgjohansson@gmail.com>
2020-07-21 17:13:38 +0200

    added intensity selection to udp-recive so it automaticly selects the strongest signal

--- top_block.py
+++ top_block.py
@@ -3,7 +3,7 @@
 ##################################################
 # GNU Radio Python Flow Graph
 # Title: udp-recive
-# Generated: Sun Jul 19 19:07:25 2020
+# Generated: Tue Jul 21 17:11:54 2020
 ##################################################
 
 from distutils.version import StrictVersion
@@ -20,15 +20,20 @@ if __name__ == '__main__':
 
 from PyQt5 import Qt
 from PyQt5 import Qt, QtCore
+from gnuradio import analog
 from gnuradio import blocks
 from gnuradio import eng_notation
+from gnuradio import filter
 from gnuradio import gr
 from gnuradio import qtgui
 from gnuradio.eng_option import eng_option
 from gnuradio.filter import firdes
+from grc_gnuradio import blks2 as grc_blks2
 from optparse import OptionParser
 import sip
 import sys
+import threading
+import time
 from gnuradio import qtgui
 
 
@@ -68,6 +73,7 @@ class top_block(gr.top_block, Qt.QWidget):
         self.samp_rate = samp_rate = 850e3
         self.dec = dec = 25
         self.freq_0 = freq_0 = 137.50625e6
+        self.channel_nr = channel_nr = 0
         self.bandwidth = bandwidth = 34e3
         self.RF_gain = RF_gain = 40
         self.Out_samprate = Out_samprate = samp_rate/dec
@@ -79,15 +85,118 @@ class top_block(gr.top_block, Qt.QWidget):
         ##################################################
         # Blocks
         ##################################################
+
+        def _channel_nr_probe():
+            while True:
+                val = self.Probe_channel.get_value()
+                try:
+                    self.set_channel_nr(val)
+                except AttributeError:
+                    pass
+                time.sleep(1.0 / (10))
+        _channel_nr_thread = threading.Thread(target=_channel_nr_probe)
+        _channel_nr_thread.daemon = True
+        _channel_nr_thread.start()
+
+        self.rational_resampler_xxx_0 = filter.rational_resampler_fff(
+                interpolation=11025,
+                decimation=34000,
+                taps=None,
+                fractional_bw=None,
+        )
+        self.qtgui_time_raster_sink_x_0 = qtgui.time_raster_sink_f(
+        	11025,
+        	1024,
+        	1024,
+        	([]),
+        	([]),
+        	"",
+        	1,
+        	)
+
+        self.qtgui_time_raster_sink_x_0.set_update_time(0.10)
+        self.qtgui_time_raster_sink_x_0.set_intensity_range(-1, 1)
+        self.qtgui_time_raster_sink_x_0.enable_grid(False)
+        self.qtgui_time_raster_sink_x_0.enable_axis_labels(True)
+
+        labels = ['', '', '', '', '',
+                  '', '', '', '', '']
+        colors = [0, 0, 0, 0, 0,
+                  0, 0, 0, 0, 0]
+        alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
+                  1.0, 1.0, 1.0, 1.0, 1.0]
+        for i in xrange(1):
+            if len(labels[i]) == 0:
+                self.qtgui_time_raster_sink_x_0.set_line_label(i, "Data {0}".format(i))
+            else:
+                self.qtgui_time_raster_sink_x_0.set_line_label(i, labels[i])
+            self.qtgui_time_raster_sink_x_0.set_color_map(i, colors[i])
+            self.qtgui_time_raster_sink_x_0.set_line_alpha(i, alphas[i])
+
+        self._qtgui_time_raster_sink_x_0_win = sip.wrapinstance(self.qtgui_time_raster_sink_x_0.pyqwidget(), Qt.QWidget)
+        self.top_layout.addWidget(self._qtgui_time_raster_sink_x_0_win)
+        self.qtgui_number_sink_0 = qtgui.number_sink(
+            gr.sizeof_short,
+            0,
+            qtgui.NUM_GRAPH_NONE,
+            1
+        )
+        self.qtgui_number_sink_0.set_update_time(0.1)
+        self.qtgui_number_sink_0.set_title("Selected channel")
+
+        labels = ['', '', '', '', '',
+                  '', '', '', '', '']
+        units = ['', '', '', '', '',
+                 '', '', '', '', '']
+        colors = [("black", "black"), ("black", "black"), ("black", "black"), ("black", "black"), ("black", "black"),
+                  ("black", "black"), ("black", "black"), ("black", "black"), ("black", "black"), ("black", "black")]
+        factor = [1, 1, 1, 1, 1,
+                  1, 1, 1, 1, 1]
+        for i in xrange(1):
+            self.qtgui_number_sink_0.set_min(i, 0)
+            self.qtgui_number_sink_0.set_max(i, 2)
+            self.qtgui_number_sink_0.set_color(i, colors[i][0], colors[i][1])
+            if len(labels[i]) == 0:
+                self.qtgui_number_sink_0.set_label(i, "Data {0}".format(i))
+            else:
+                self.qtgui_number_sink_0.set_label(i, labels[i])
+            self.qtgui_number_sink_0.set_unit(i, units[i])
+            self.qtgui_number_sink_0.set_factor(i, factor[i])
+
+        self.qtgui_number_sink_0.enable_autoscale(False)
+        self._qtgui_number_sink_0_win = sip.wrapinstance(self.qtgui_number_sink_0.pyqwidget(), Qt.QWidget)
+        self.top_layout.addWidget(self._qtgui_number_sink_0_win)
+        self.probe_channel = blocks.probe_signal_s()
+        self.blocks_wavfile_sink_0 = blocks.wavfile_sink('/home/david/Music/foobar', 1, 11025, 16)
         self.blocks_udp_source_0_2 = blocks.udp_source(gr.sizeof_gr_complex*1, '0.0.0.0', 1232, 1472, True)
         self.blocks_udp_source_0_1 = blocks.udp_source(gr.sizeof_gr_complex*1, '0.0.0.0', 1230, 1472, True)
         self.blocks_udp_source_0 = blocks.udp_source(gr.sizeof_gr_complex*1, '0.0.0.0', 1231, 1472, True)
+        self.blocks_streams_to_vector_0 = blocks.streams_to_vector(gr.sizeof_float*1, 3)
+        self.blocks_null_sink_0 = blocks.null_sink(gr.sizeof_short*1)
+        self.blocks_moving_average_2_0 = blocks.moving_average_ff(4000, 1, 4000)
+        self.blocks_moving_average_2 = blocks.moving_average_ff(4000, 1, 4000)
+        self.blocks_moving_average_1 = blocks.moving_average_ff(4000, 1, 4000)
+        self.blocks_complex_to_mag_squared_0_1 = blocks.complex_to_mag_squared(1)
+        self.blocks_complex_to_mag_squared_0_0 = blocks.complex_to_mag_squared(1)
+        self.blocks_complex_to_mag_squared_0 = blocks.complex_to_mag_squared(1)
+        self.blocks_argmax_xx_0 = blocks.argmax_fs(3)
+        self.blks2_selector_0 = grc_blks2.selector(
+        	item_size=gr.sizeof_gr_complex*1,
+        	num_inputs=3,
+        	num_outputs=1,
+        	input_index=channel_nr,
+        	output_index=0,
+        )
+        self.analog_wfm_rcv_0 = analog.wfm_rcv(
+        	quad_rate=bandwidth,
+        	audio_decimation=1,
+        )
         self.NOAA19_fall = qtgui.sink_c(
         	1024, #fftsize
         	firdes.WIN_BLACKMAN_hARRIS, #wintype
         	freq_0+NOAA19, #fc
         	Out_samprate, #bw
-        	"NOAA19", #name
+        	"NOAA19 (stream 0)", #name
         	True, #plotfreq
         	True, #plotwaterfall
         	False, #plottime
@@ -106,7 +215,7 @@ class top_block(gr.top_block, Qt.QWidget):
         	firdes.WIN_BLACKMAN_hARRIS, #wintype
         	freq_0+NOAA18, #fc
         	Out_samprate, #bw
-        	"NOAA18", #name
+        	"NOAA18 (stream 1)", #name
         	True, #plotfreq
         	True, #plotwaterfall
         	False, #plottime
@@ -125,7 +234,7 @@ class top_block(gr.top_block, Qt.QWidget):
         	firdes.WIN_BLACKMAN_hARRIS, #wintype
         	freq_0+NOAA15, #fc
         	samp_rate, #bw
-        	"NOAA15", #name
+        	"NOAA15 (stream 2)", #name
         	True, #plotfreq
         	True, #plotwaterfall
         	False, #plottime
@@ -143,9 +252,29 @@ class top_block(gr.top_block, Qt.QWidget):
         ##################################################
         # Connections
         ##################################################
+        self.connect((self.analog_wfm_rcv_0, 0), (self.rational_resampler_xxx_0, 0))
+        self.connect((self.blks2_selector_0, 0), (self.analog_wfm_rcv_0, 0))
+        self.connect((self.blocks_argmax_xx_0, 1), (self.blocks_null_sink_0, 0))
+        self.connect((self.blocks_argmax_xx_0, 0), (self.probe_channel, 0))
+        self.connect((self.blocks_argmax_xx_0, 0), (self.qtgui_number_sink_0, 0))
+        self.connect((self.blocks_complex_to_mag_squared_0, 0), (self.blocks_moving_average_1, 0))
+        self.connect((self.blocks_complex_to_mag_squared_0_0, 0), (self.blocks_moving_average_2, 0))
+        self.connect((self.blocks_complex_to_mag_squared_0_1, 0), (self.blocks_moving_average_2_0, 0))
+        self.connect((self.blocks_moving_average_1, 0), (self.blocks_streams_to_vector_0, 0))
+        self.connect((self.blocks_moving_average_2, 0), (self.blocks_streams_to_vector_0, 1))
+        self.connect((self.blocks_moving_average_2_0, 0), (self.blocks_streams_to_vector_0, 2))
+        self.connect((self.blocks_streams_to_vector_0, 0), (self.blocks_argmax_xx_0, 0))
         self.connect((self.blocks_udp_source_0, 0), (self.NOAA19_fall, 0))
+        self.connect((self.blocks_udp_source_0, 0), (self.blks2_selector_0, 0))
+        self.connect((self.blocks_udp_source_0, 0), (self.blocks_complex_to_mag_squared_0, 0))
         self.connect((self.blocks_udp_source_0_1, 0), (self.NOAA18_fall, 0))
+        self.connect((self.blocks_udp_source_0_1, 0), (self.blks2_selector_0, 1))
+        self.connect((self.blocks_udp_source_0_1, 0), (self.blocks_complex_to_mag_squared_0_0, 0))
         self.connect((self.blocks_udp_source_0_2, 0), (self.NOAA15_fall, 0))
+        self.connect((self.blocks_udp_source_0_2, 0), (self.blks2_selector_0, 2))
+        self.connect((self.blocks_udp_source_0_2, 0), (self.blocks_complex_to_mag_squared_0_1, 0))
+        self.connect((self.rational_resampler_xxx_0, 0), (self.blocks_wavfile_sink_0, 0))
+        self.connect((self.rational_resampler_xxx_0, 0), (self.qtgui_time_raster_sink_x_0, 0))
 
     def closeEvent(self, event):
         self.settings = Qt.QSettings("GNU Radio", "top_block")
@@ -175,6 +304,13 @@ class top_block(gr.top_block, Qt.QWidget):
         self.NOAA19_fall.set_frequency_range(self.freq_0+self.NOAA19, self.Out_samprate)
         self.NOAA18_fall.set_frequency_range(self.freq_0+self.NOAA18, self.Out_samprate)
         self.NOAA15_fall.set_frequency_range(self.freq_0+self.NOAA15, self.samp_rate)
+
+    def get_channel_nr(self):
+        return self.channel_nr
+
+    def set_channel_nr(self, channel_nr):
+        self.channel_nr = channel_nr
+        self.blks2_selector_0.set_input_index(int(self.channel_nr))
 
     def get_bandwidth(self):
         return self.bandwidth
